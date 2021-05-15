80aa325c7fc9abd1f1c111f70889701ecab5ce1f
K Venkata Naga Alekhya <67037987+alekhya2202@users.noreply.github.com>
2021-05-15 17:32:36 +0530

    Create Day8_Dictionaries_and_maps.java

---
+++ Day8_Dictionaries_and_maps.java
@@ -0,0 +1,31 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        Map<String, Integer> phoneBook = new HashMap<String, Integer>();
+        
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            phoneBook.put(name, phone);
+        }
+        
+        
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if (phoneBook.get(s) == null){
+                System.out.println("Not found");
+            } 
+            else {
+                System.out.println(s+ "=" + phoneBook.get(s));}
+        }
+    
+        in.close();
+    }
